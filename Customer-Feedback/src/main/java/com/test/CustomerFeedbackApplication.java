24f801ac87a9fa6bf5ac378bc568fa91bf4b9b8d
NandkumarBankar <112711500+NandkumarBankar@users.noreply.github.com>
2025-01-08 18:06:42 +0530

    Update CustomerFeedbackApplication.java

--- Customer-Feedback/src/main/java/com/test/CustomerFeedbackApplication.java
+++ Customer-Feedback/src/main/java/com/test/CustomerFeedbackApplication.java
@@ -9,6 +9,7 @@ public class CustomerFeedbackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerFeedbackApplication.class, args);
+			System.out.println("Updated Jenkins Test : 1");
 	}
 
 }
